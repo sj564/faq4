7611537e2d5f703ecf052c6d6d0340ea04c64c26
siddharth <sj564@njit.edu>
2018-05-05 05:15:27 -0400

    >Created a new branch AccessControl to assign different access authorizations to different types of users,i.e admin,tutor, and student.
    >Added user role in users table

--- app/User.php
+++ app/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-         'email', 'password',
+         'email', 'password','user_role'
     ];
 
     /**
@@ -30,7 +30,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'user_role',
     ];
     public function profile()
     {
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('user_role',100)->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
