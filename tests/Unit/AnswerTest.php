e1cc661517a68c3a5b5b2553640725810fd4a626
siddharth <sj564@njit.edu>
2018-05-07 19:21:41 -0400

    Added Insert,Delete Question and Insert,delete Answer tests

--- tests/Unit/AnswerTest.php
+++ tests/Unit/AnswerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Answer;
 
 class AnswerTest extends TestCase
 {
@@ -20,5 +21,27 @@ class AnswerTest extends TestCase
         $answer->user()->associate($user);
         $answer->question()->associate($question);
         $this->assertTrue($answer->save());
+    }
+
+    public function testInsertAnswer()
+    {
+        $answer = new Answer();
+        $answer->user_id = '125';
+        $answer->question_id = '625';
+        $answer->body = 'What is the capital of El Salvador?';
+
+        $answer->save();
+        $this->assertTrue($answer->save());
+    }
+
+    public function testDeleteAnswer()
+    {
+        $answer = new Answer();
+        $answer->user_id = '190';
+        $answer->question_id = '378';
+        $answer->body = 'What is the area of Paraguay?';
+
+        $answer->save();
+        $this->assertTrue($answer->delete());
     }
 }
--- tests/Unit/QuestionTest.php
+++ tests/Unit/QuestionTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Question;
 
 class QuestionTest extends TestCase
 {
@@ -16,5 +17,25 @@ class QuestionTest extends TestCase
         $question = factory(\App\Question::class)->make();
         $question->user()->associate($user);
         $this->assertTrue($question->save());
+    }
+
+    public function testInsertQuestion()
+    {
+        $question = new Question();
+        $question->user_id = '620';
+        $question->body = 'What is the capital Ukraine?';
+
+        $question->save();
+        $this->assertTrue($question->save());
+    }
+
+    public function testDeleteQuestion()
+    {
+        $question = new Question();
+        $question->user_id = '765';
+        $question->body = 'What is the capital South Africa?';
+
+        $question->save();
+        $this->assertTrue($question->delete());
     }
 }
